72bdb8ea84f9e7a29e4f6df1325ae540b2579cfa
superduperblade <catmasterking@gmail.com>
2025-12-16 16:51:13 +0100

    added functionality to switch between contacts

--- client/src/app/compoments/Contact.tsx
+++ client/src/app/compoments/Contact.tsx
@@ -1,14 +1,16 @@
 import React, { ReactNode } from 'react'
+import type { ComponentType } from 'react'
 interface Props{
     contact_name: string
     children: ReactNode
+    onClick: () => void;
 }
-const Contact = ({contact_name,children}: Props) => {
+const Contact = ({contact_name,children,onClick}: Props) => {
     
   return (
-    <li className="list-group-item flex">
-        <div className='contact-header fs-5'>{contact_name}</div>
-        <div className='last-Message-text fs-6'>Last message: {children}</div>
+    <li className="list-group-item flex" onClick={onClick}>
+        <div className='contact-header fs-5' onClick={onClick}>{contact_name}</div>
+        <div className='last-Message-text fs-6' onClick={onClick}>Last message: {children}</div>
     </li>
   )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import Contact from "./compoments/Contact";
 import Message from "./compoments/Message";
-
+import MessageBox from "./compoments/Message_box";
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [activeContact, setActiveContact] = useState<string | null>(null);
+
+
+
   return (
     <div className="container-fluid vh-100">
       <div className="row h-100">
@@ -14,41 +22,22 @@ export default function Home() {
           </div>
 
           <ul className="list-group list-group-flush flex-grow-1 overflow-auto">
-            <Contact contact_name='bob' > Hello</Contact>
-            <Contact contact_name='Phil' > How are you</Contact>
-            <Contact contact_name='Andy' > Is there any plans today</Contact>
+            <Contact contact_name='bob' onClick={() => setActiveContact("bob")}> Hello</Contact>
+            <Contact contact_name='Phil' onClick={() => setActiveContact("Phil")}> How are you</Contact>
+            <Contact contact_name='Andy' onClick={() => setActiveContact("Andy")}> Is there any plans today</Contact>
           </ul>
         </aside>
-
-        {/* Message area */}
-        <main className="col-9 d-flex flex-column p-0">
-
-          {/* Chat header */}
-          <div className="p-3 border-bottom fw-bold">
-            Alice
-          </div>
-
-          {/* Messages */}
-          <ul className="flex-grow-1 p-3 overflow-auto bg-dark no-bottom-padding">
-           <Message home_user="Phil" contact_name="Alice">How are you</Message>
-           <Message home_user="Phile" contact_name="Phile">I am fine</Message>
-           <Message home_user="Phil" contact_name="Alice">Thats good</Message>
-          </ul>
-
-          {/* Input box */}
-          <div className="p-3 border-top">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Type a message…"
-              />
-              <button className="btn btn-primary">
-                Send
-              </button>
-            </div>
-          </div>
-
+         <main className="col-9 d-flex flex-column p-0">
+          {activeContact && (
+            <MessageBox>
+              <Message
+                contact_name={activeContact}
+                home_user="phile"
+              >
+                Hello
+              </Message>
+            </MessageBox>
+          )}
         </main>
       </div>
     </div>
