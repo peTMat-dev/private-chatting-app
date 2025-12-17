019257a9460353dfad9764fc578411ef450df4eb
superduperblade <catmasterking@gmail.com>
2025-12-17 11:18:17 +0100

    database connection

--- server/Main.js
+++ server/Main.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+
+const mySql = require('mysql')
 const express = require('express')
 const app = express();
 const cors = require("cors");
@@ -7,10 +10,27 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
-app.get("/api",(req,res) =>{
-    res.json({"fruits": ["apple","test","orange"]})
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
+
+connection.connect()
+
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.json({"Api-Server": 200,
+        "Database-Connection" : "unknown"
+    });
+})
+
+
 
 app.listen(8080, () => {
     console.log("Server has started on port 8080");
