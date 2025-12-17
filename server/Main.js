7c71f5885a5f98d4a09ed848098b8ea93bd3be08
superduperblade <catmasterking@gmail.com>
2025-12-17 11:45:09 +0100

    added a check to see if the database is alive

--- server/Main.js
+++ server/Main.js
@@ -10,23 +10,36 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const connection = mysql.createConnection({
+
+let isalive = 400;
+const conPool = mySql.createPool({
+
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 })
 
-connection.connect()
 
 
+//checking if the database is alive
+function checkDB(){
+    conPool.query('SELECT 1',(err) => {
+        if (err){
+            isalive = 400;
+        }else{
+            isalive = 200;
+        }
+    })
+}
+setInterval(checkDB,20000);
 
 
 
 
 app.get("/",(req,res)=>{
     res.json({"Api-Server": 200,
-        "Database-Connection" : "unknown"
+        "Database-Connection" : isalive
     });
 })
 
