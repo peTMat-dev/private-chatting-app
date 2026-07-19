3b626c0570605f41a234ed6c9bd7d3e4a4377528
peTMat-dev <peter.blox123@gmail.com>
2026-07-19 15:20:32 +0200

    gitignore folder changes

--- gitignore/ignore.sh
+++
@@ -1,7 +0,0 @@
-echo ".claude/" >> .gitignore
-git add .gitignore
-git rm -r --cached .claude
-git commit -m "Stop tracking .claude"
-
-git add -A
-git commit -m "Stop tracking ignored files and update .gitignore"
